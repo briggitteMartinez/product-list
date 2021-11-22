13f52a04d653269d4c3cc5ec6cf68bf580c931d6
briggitteMartinez <briggitte90@hotmail.com>
2021-11-22 14:26:00 +0100

    added edit screen

---
+++ src/screens/EditProductScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const EditProductScreen = () => {
+    return (
+        <View>
+            <Text> newproductscreen</Text>
+        </View>
+    )
+};
+export default EditProductScreen;
