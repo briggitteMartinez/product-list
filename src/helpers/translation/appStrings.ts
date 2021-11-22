26b5414a1ad5afcbc00bd50d09f8686447de25fa
briggitteMartinez <briggitte90@hotmail.com>
2021-11-22 18:58:25 +0100

    added UI to first screen

--- src/helpers/translation/appStrings.ts
+++ src/helpers/translation/appStrings.ts
@@ -6,12 +6,15 @@ enum NewProductScreen{
 }
 
 enum ProductListScreen{
-    mainText = "productListScreen-main-text"
+    text1 = "productListScreen-text1",
+    text2 = "productListScreen-text2",
+    text3 = "productListScreen-text3",
+    text4 = "productListScreen-text4",
 
 }
 
 enum EditProductListScreen{
-    mainText = "editProductListScreen-main-text"
+    text1 = "editProductListScreen-main-text"
 
 }
 
@@ -19,5 +22,6 @@ export const tokens = {
     screens:{
         productListScreen : ProductListScreen,
         newProductScreen : NewProductScreen,
+        editProductListScreen : EditProductListScreen,
     }
 }
--- src/helpers/translation/en.ts
+++ src/helpers/translation/en.ts
@@ -1,7 +1,11 @@
 import { tokens } from "./appStrings";
 
 export const english = {
-    [tokens.screens.productListScreen.mainText]:"",
+    [tokens.screens.productListScreen.text1]:"name",
+    [tokens.screens.productListScreen.text2]:"Type",
+    [tokens.screens.productListScreen.text3]:"Price",
+    [tokens.screens.productListScreen.text4]:"You do not have any products. Press the green button below to add a new one",
+
     [tokens.screens.newProductScreen.mainText]:"",
 
 }
